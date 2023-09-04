cea540e185fc3955ceacf85512086a2dccfd08aa
X-Ai-G79 <144046345+X-Ai-G79@users.noreply.github.com>
2023-09-04 21:37:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 function addTask() {
     const taskInput = document.getElementById("new-task");
     const taskText = taskInput.value.trim();
+    const categorySelect = document.getElementById("category");
+    const categoryText = categorySelect.value;
 
     if (taskText === "") {
         return;
     }
 
+    let categoryBox = document.getElementById(categoryText);
+
+    if (!categoryBox) {
+        categoryBox = document.createElement("div");
+        categoryBox.id = categoryText;
+        categoryBox.className = 'category-box';
+        const categoryLabel = document.createElement("h2");
+        categoryLabel.innerText = categoryText;
+        categoryBox.appendChild(categoryLabel);
+        const taskList = document.createElement("ul");
+        categoryBox.appendChild(taskList);
+        document.getElementById("task-list").appendChild(categoryBox);
+    }
+
+    const taskList = categoryBox.querySelector("ul");
     const taskElement = document.createElement("li");
     taskElement.innerText = taskText;
 
@@ -20,7 +37,6 @@ function addTask() {
     });
 
     taskElement.appendChild(deleteButton);
-
-    document.getElementById("task-list").appendChild(taskElement);
+    taskList.appendChild(taskElement);
     taskInput.value = "";
 }
